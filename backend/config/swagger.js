98a7fd6b92b2a52ce9a64261e301331c2e532b27
Vrutik <vrutikprajapati2809@gmail.com>
2023-04-01 18:21:19 +0530

    Add swagger configuration to the project

---
+++ backend/config/swagger.js
@@ -0,0 +1,46 @@
+export default {
+    openapi: "3.0.0",
+    info: {
+        title: "Menulize Backend APIs",
+        description: "All Apis for system",
+        version: "0.0.1",
+        license: {
+            name: "Vrutik Prajapati",
+            url: "https://github.com/vrutik2809",
+        },
+    },
+    servers: [
+        {
+            url: "http://localhost:8080",
+            description: "Development server",
+        },
+    ],
+    tags: [
+        {
+            name: "Auth",
+            description: "API to manage authentication",
+        },
+        {
+            name: "User",
+            description: "API to manage user routes",
+        }
+    ],
+    components: {
+        securitySchemes: {
+            Bearer: {
+                type: "apiKey",
+                bearerFormat: "JWT",
+                in: "header",
+                name: "Authorization",
+                description: "Enter the token with the `Bearer` prefix, e.g. 'Bearer jwt-token-here'",
+            },
+        },
+        schemas: {
+           
+        },
+    },
+    paths: {
+       
+    },
+    apis: [ "./routes/*.js" ],
+}
