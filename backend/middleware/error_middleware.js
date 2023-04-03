cf7e786b13a849b6b5efa1eec1f7995ef59554ba
swadheenta20 <58162518+swadheenta20@users.noreply.github.com>
2023-04-03 23:20:09 +0530

    errorHandler() function created

--- backend/middleware/error_middleware.js
+++ backend/middleware/error_middleware.js
@@ -8,3 +8,12 @@ exports.notFound = (req, res, next) => {
     next(error);
 };
 
+//error handler. code 200 and 500 represent erros here. 
+exports.errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+};
