ac4f00e5ca59fec78569b895f1c82ebe156098d9
Nishadh4014 <nishadhshah1249@gmail.com>
2023-04-03 23:23:31 +0530

    login function created in userActions.jsgit add .

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import {
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_FAIL,
+    USER_LOGIN_SUCCESS,
+} from "./../constants/userConstants";
+
+//login
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST,
+        });
+
+        //headers
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        //get login data
+        const { data } = await axios.post(
+            "/api/users/login",
+            { email, password },
+            config
+        );
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data,
+        });
+
+        //set user info into local storage
+        localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
