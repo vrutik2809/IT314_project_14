d1916b69b1152eeba1ed97d8e915f0dcb5d32ae5
harshb0310 <77343146+harshb0310@users.noreply.github.com>
2023-04-04 23:55:12 +0530

    Init React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+};
+
+export default App;
