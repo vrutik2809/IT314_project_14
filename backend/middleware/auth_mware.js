2965fb7e0d265ce7d227d73f1288ca813d2e45ee
Shreyansh Khemesara <202001121@daiict.ac.in>
2023-04-04 20:41:24 +0530

    Indentation and comments-auth

--- backend/middleware/auth_mware.js
+++ backend/middleware/auth_mware.js
@@ -1,25 +1,26 @@
 import jwt from "jsonwebtoken"
 import User from "../models/users.js"
-import User from "../models/users.js"
 
 export const protect = async (req, res, next) => {
-    let token
     try {
         let token
+
         if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
             //[0] = Bearer , [1] = token
             token = req.headers.authorization.split(" ")[1]
+
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
             req.user = await User.findOne({ _id: decoded._id, }).select("-password")
             if (!req.user) {
                 return res.status(401).json({ message: "Not authorized", })
             }
             next()
         }
+
         if (!token) {
             return res.status(401).json({ message: "Not authorized", })
         }
-        next()
     } catch (error) {
         console.log(error)
         if (error.name === "TokenExpiredError") {
@@ -30,10 +31,6 @@ export const protect = async (req, res, next) => {
         }
         return res.status(500).json({ message: "Internal Server Error", })
     }
-    if (!token) {
-        return res.status(401).json({ message: "Not authorized", })
-    } 
-
 }
 
 export const admin = async (req, res, next) => {
@@ -43,4 +40,3 @@ export const admin = async (req, res, next) => {
         return res.status(401).json({ message: "Not authorized as an admin", })
     }
 }
-    
