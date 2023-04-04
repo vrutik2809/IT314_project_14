f50e845573b7e3dba7b76748fac2ca18e2cde5bb
harshb0310 <77343146+harshb0310@users.noreply.github.com>
2023-04-05 00:05:03 +0530

    Error Page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
-
+import Error from "./Error";
 const App = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route path = "*" component={Error} />
             </Switch>
         </Router>
     );
