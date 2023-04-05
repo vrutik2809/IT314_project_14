a5e56d59e0c3520dee519016df01a9dee9ccb801
Nishadh4014 <nishadhshah1249@gmail.com>
2023-04-05 11:09:46 +0530

    got user creds for login and authenticated

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -9,3 +9,36 @@ import {
 } from "./../constants/userConstants";
 
 // handling login backend
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST,
+        });
+
+        //headers
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        //get login data
+        const { data } = await axios.post(
+            "/api/auth/login",
+            { email, password },
+            config
+        );
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
