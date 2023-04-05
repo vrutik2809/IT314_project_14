cb3befadd32b352f90aea1e7e8478404755bb440
Krunal Chhagani <kunalthakkar964@gmail.com>
2023-04-05 22:35:03 +0530

    login controller is mapped to the route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,9 @@
 import { Router } from 'express'
 
+import * as controller from '../controllers/auth.js'
+
 const router = Router()
+
+router.post('/login',controller.login)
 
 export default router
