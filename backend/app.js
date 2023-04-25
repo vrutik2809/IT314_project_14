ea2128bb2b3e97ea89b59873861dfde1f1a80e42
Nishadh4014 <nishadhshah1249@gmail.com>
2023-04-25 22:45:46 +0530

    finally updated server file

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import swaggerUi from 'swagger-ui-express'
 import swaggerDocument from './config/swagger.js'
 import authRoutes from './routes/auth.js'
 import categoryRoutes from './routes/category.js'
+import productRoutes from './routes/product.js'
 
 dotenv.config()
 
@@ -28,6 +29,7 @@ app.get('/', async (req, res) => {
 
 app.use('/api/auth', authRoutes)
 app.use('/api/categories', categoryRoutes)
+app.use('/api/products', productRoutes)
 // 404 route
 app.use((req, res) => {
     res.status(404).json({ message: 'Not Found', })
