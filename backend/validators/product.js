5e55fbf57207e82b2e52c804789604d4524c45d8
Nishadh4014 <nishadhshah1249@gmail.com>
2023-04-25 22:42:32 +0530

    validating mongo database

--- backend/validators/product.js
+++ backend/validators/product.js
@@ -1,2 +1,10 @@
 import joi from 'joi'
 import mongoose from "mongoose"
+
+const validateMongoID = (value, helper) => {
+    const isValidMongoId = mongoose.Types.ObjectId.isValid
+    
+    if(!isValidMongoId(value)){
+        return helper.message('Invalid id')
+    }
+}
