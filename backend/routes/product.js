d07e8d077f4ef76eb09c2aed2616a5fbe198d11d
Nishadh4014 <nishadhshah1249@gmail.com>
2023-04-25 22:37:32 +0530

    four operations added create, read, update and delete

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,3 +6,13 @@ import validation from '../middlewares/validation.js'
 import * as product from '../validators/product.js'
 
 const router = Router()
+
+router.get('/', [ protect ], controller.getAllProducts)
+
+router.post('/', [ protect, validation(product.bodySchema) ], controller.createProduct)
+
+router.get('/:id', [ protect, validation(product.paramSchema, 'params') ], controller.getProduct)
+
+router.put('/:id', [ protect, validation(product.paramSchema, 'params'), validation(product.bodySchema) ], controller.updateProduct)
+
+router.delete('/:id', [ protect, validation(product.paramSchema, 'params') ], controller.deleteProduct)
