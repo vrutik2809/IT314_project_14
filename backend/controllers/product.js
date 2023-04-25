2f449abe359b27c7a22f73ebcf2c9359455f664d
Nishadh4014 <nishadhshah1249@gmail.com>
2023-04-25 23:03:28 +0530

    reading the information of product

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -54,3 +54,25 @@ export const createProduct = async (req, res) => {
         return res.status(500).json({ message: 'Internal server error', })
     }
 }
+
+export const getProduct = async (req, res) => {
+    try {
+        let product = await Product.findById(req.params.id).populate({
+            path: 'category_id',
+            select: 'name -_id',
+        })
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found', })
+        }
+        const { _id,category_id, ...rest } = product._doc
+        product = {
+            id: _id,
+            category: category_id,
+            ...rest,
+        }
+        return res.status(200).json(product)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: 'Internal server error', })
+    }
+}
