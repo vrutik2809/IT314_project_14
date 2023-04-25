a691f49e6aa43276094b44f3583178b82fddf439
Nishadh4014 <nishadhshah1249@gmail.com>
2023-04-25 23:04:53 +0530

    deleting product

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -103,3 +103,16 @@ export const updateProduct = async (req, res) => {
         return res.status(500).json({ message: 'Internal server error', })
     }
 }
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id)
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found', })
+        }
+        return res.status(200).json({ message: 'Product deleted', })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: 'Internal server error', })
+    }
+}
