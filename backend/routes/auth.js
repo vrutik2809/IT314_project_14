a118fd0648d3108cd99a41f0d2119f2e42d6b309
Vrutik <vrutikprajapati2809@gmail.com>
2023-04-25 16:29:32 +0530

    Add middleware in the  `login` route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 
 import * as controller from '../controllers/auth.js'
+import validate from '../middlewares/validation.js'
+import * as auth from '../validators/auth.js'
 
 const router = Router()
 
-router.post('/login',controller.login)
+router.post('/login',[ validate(auth.bodySchema) ],controller.login)
 
 export default router
