5bf8db8ba256e11fcdd0fa8cd52ffc923f5ce1fd
harshb0310 <77343146+harshb0310@users.noreply.github.com>
2023-04-26 23:19:48 +0530

    fixed profile screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -6,6 +6,12 @@ import {
     USER_LOGIN_SUCCESS,
     USER_LOGOUT,
     USER_DETAILS_RESET,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAIL,
 } from "./../constants/userConstants";
 
 // handling login backend
@@ -39,6 +45,79 @@ export const login = (email, password) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: USER_LOGIN_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const listUserDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({ type: USER_DETAILS_REQUEST });
+
+        //get user from state
+        const {
+            userLogin: { userInfo },
+        } = getState();
+
+        //headers
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        };
+
+        //api call to get product
+        const { data } = await axios.get(`/api/users/${id}`, config);
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+//update profile
+export const updateProfile = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_REQUEST,
+        });
+
+        //get user from state
+        const {
+            userLogin: { userInfo },
+        } = getState();
+        //headers
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        };
+
+        //update user
+        const { data } = await axios.put(
+            `/api/users/profile/${user.id}`,
+            user,
+            config
+        );
+        dispatch({
+            type: USER_UPDATE_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: USER_UPDATE_FAIL,
             payload:
                 error.response && error.response.data.message
                     ? error.response.data.message
