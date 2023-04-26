18c5ae23b20277353d53a8525a73c62ea4991d97
Vrutik <vrutikprajapati2809@gmail.com>
2023-04-26 14:38:14 +0530

    Add orderSchema in index file

--- backend/docs/schemas/index.js
+++ backend/docs/schemas/index.js
@@ -3,6 +3,7 @@ import responsesSchema from './responses.js'
 import categorySchema from './category.js'
 import tableSchema from './table.js'
 import productSchema from './product.js'
+import orderSchema from './order.js'
 
 export default {
     ...authSchema,
@@ -10,4 +11,5 @@ export default {
     ...categorySchema,
     ...tableSchema,
     ...productSchema,
+    ...orderSchema,
 }
