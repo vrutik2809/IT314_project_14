9393e202416fec0f787e6a0064475883c3851bd5
Vrutik <vrutikprajapati2809@gmail.com>
2023-04-26 14:39:42 +0530

    Add orderPaths in index file

--- backend/docs/paths/index.js
+++ backend/docs/paths/index.js
@@ -2,10 +2,12 @@ import authPaths from './auth.js'
 import categoryPaths from './category.js'
 import tablePaths from './table.js'
 import productPaths from './product.js'
+import orderPaths from './order.js'
 
 export default {
     ...authPaths,
     ...categoryPaths,
     ...tablePaths,
     ...productPaths,
+    ...orderPaths,
 }
